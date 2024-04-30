38a369b1eeb639a4cd8faec4961e0e3fb4ed3da9
bush <busra_hekim17@outlook.com>
2024-05-01 00:16:46 +0300

    initialize project

---
+++ context/appThemeContext.tsx
@@ -0,0 +1,4 @@
+import LightTheme from "@/themes/LightTheme";
+import { createContext } from "react";
+
+export const AppThemeContext = createContext(LightTheme)
---
+++ themes/LightTheme.tsx
@@ -0,0 +1,14 @@
+import Colors from "@/constants/Colors";
+
+const LightTheme = {
+  isDark: false,
+  colors: {
+    background: Colors.background,
+    text: "#000",
+    tabbarBackground: "#FFF",
+    tabbarActiveColor: "orange",
+    tabbarInactiveColor: "#000",
+  },
+};
+
+export default LightTheme;
